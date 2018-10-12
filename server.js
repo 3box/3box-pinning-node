88a30a08a8a45fdeab84240abbfc1ec87088d8ec
Joel Torstensson <oed3933@gmail.com>
2018-10-12 12:02:23 +0200

    Use odb-pubsub in paralell to pubsub-room

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const IPFS = require('ipfs')
 const OrbitDB = require('orbit-db')
+const Pubsub = require('orbit-db-pubsub')
 const Room = require('ipfs-pubsub-room')
 
 const ipfsOptions = {
@@ -9,16 +10,20 @@ const ipfsOptions = {
     pubsub: true
   }
 }
+const ORBITDB_PATH = '/opt/orbitdb'
+const PINNING_ROOM = '3box-pinning'
 
 // Create IPFS instance
 const ipfs = new IPFS(ipfsOptions)
-let storeDBs = []
 
 ipfs.on('error', (e) => console.error(e))
 ipfs.on('ready', async () => {
   console.log(await ipfs.id())
   const room = Room(ipfs, '3box')
-  const orbitdb = new OrbitDB(ipfs, '/opt/orbitdb')
+  const orbitdb = new OrbitDB(ipfs, ORBITDB_PATH)
+  const pubsub = new Pubsub(ipfs, (await ipfs.id()).id)
+
+  pubsub.subscribe(PINNING_ROOM, onMessage, onNewPeer)
 
   room.on('peer joined', (peer) => {
     console.log('Peer joined the room', peer)
@@ -35,8 +40,11 @@ ipfs.on('ready', async () => {
   room.on('message', async (message) => {
     let msgData = message.data.toString()
     console.log('incoming message', msgData)
+    openRootDB(msgData)
+  })
 
-    const rootDB = await orbitdb.open(msgData)
+  async function openRootDB (address) {
+    const rootDB = await orbitdb.open(address)
     const readyPromise = new Promise((resolve, reject) => {
       rootDB.events.on('ready', () => {
         rootDB.iterator({ limit: -1 }).collect().map(entry => {
@@ -62,25 +70,32 @@ ipfs.on('ready', async () => {
       }
     )
     await readyPromise
+  }
 
-    async function openDB (address, onProgress = () => { }) {
-      console.log('Opening db:', address)
-      let db = await orbitdb.open(address)
-      db.load()
-      storeDBs.push(db)
-      db.events.on(
-        'replicate.progress',
-        (odbAddress, entryHash, entry, num, max) => {
-          onProgress(entry)
-          console.log('Replicating entry:', entryHash)
-          console.log('On db:', odbAddress)
-          if (num === max) {
-            db.events.on('replicated', () => {
-              console.log('Fully replicated db:', odbAddress)
-            })
-          }
+  async function openDB (address) {
+    console.log('Opening db:', address)
+    let db = await orbitdb.open(address)
+    db.load()
+    db.events.on(
+      'replicate.progress',
+      (odbAddress, entryHash, entry, num, max) => {
+        console.log('Replicating entry:', entryHash)
+        console.log('On db:', odbAddress)
+        if (num === max) {
+          db.events.on('replicated', () => {
+            console.log('Fully replicated db:', odbAddress)
+          })
         }
-      )
-    }
-  })
+      }
+    )
+  }
+
+  async function onMessage (topic, data) {
+    console.log(topic, data)
+    openRootDB(data.odbAddress)
+  }
+
+  async function onNewPeer (topic, peer) {
+    console.log('peer joined room', topic, peer)
+  }
 })
