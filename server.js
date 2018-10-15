cf736e3cbd1f85b4c3c99abc427d8cc461d7b216
Joel Torstensson <oed3933@gmail.com>
2018-10-15 09:57:55 +0200

    Cleaned up code and added pubsub responses

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ const PINNING_ROOM = '3box-pinning'
 // Create IPFS instance
 const ipfs = new IPFS(ipfsOptions)
 
+let openDBs = {}
+
 ipfs.on('error', (e) => console.error(e))
 ipfs.on('ready', async () => {
   console.log(await ipfs.id())
@@ -44,55 +46,86 @@ ipfs.on('ready', async () => {
   })
 
   async function openRootDB (address) {
-    const rootDB = await orbitdb.open(address)
-    const readyPromise = new Promise((resolve, reject) => {
-      rootDB.events.on('ready', () => {
-        rootDB.iterator({ limit: -1 }).collect().map(entry => {
-          const odbAddress = entry.payload.value.odbAddress
-          openDB(odbAddress)
-        })
-        resolve()
+    if (!openDBs[address]) {
+      openDBs[address] = await orbitdb.open(address)
+      openDBs[address].events.on('ready', () => {
+        openStoresAndSendResponse()
       })
-    })
-    rootDB.load()
+      openDBs[address].load()
 
-    rootDB.events.on(
-      'replicate.progress',
-      (odbAddress, entryHash, entry, num, max) => {
-        openDB(entry.payload.value.odbAddress)
-        console.log('Replicating entry:', entryHash)
-        console.log('On db:', odbAddress)
-        if (num === max) {
-          rootDB.events.on('replicated', () => {
-            console.log('Fully replicated db:', odbAddress)
-          })
+      openDBs[address].events.on(
+        'replicate.progress',
+        (odbAddress, entryHash, entry, num, max) => {
+          openDB(entry.payload.value.odbAddress)
+          console.log('Replicating entry:', entryHash)
+          console.log('On db:', odbAddress)
+          if (num === max) {
+            openDBs[address].events.on('replicated', () => {
+              console.log('Fully replicated db:', odbAddress)
+              publish('REPLICATED', address)
+            })
+          }
         }
-      }
-    )
-    await readyPromise
+      )
+    } else {
+      openStoresAndSendResponse()
+    }
+
+    function openStoresAndSendResponse () {
+      const numEntries = openDBs[address]._oplog._length
+      publish('HAS_ENTRIES', address, numEntries)
+      openDBs[address].iterator({ limit: -1 }).collect().map(entry => {
+        const odbAddress = entry.payload.value.odbAddress
+        openDB(odbAddress)
+      })
+    }
   }
 
   async function openDB (address) {
-    console.log('Opening db:', address)
-    let db = await orbitdb.open(address)
-    db.load()
-    db.events.on(
-      'replicate.progress',
-      (odbAddress, entryHash, entry, num, max) => {
-        console.log('Replicating entry:', entryHash)
-        console.log('On db:', odbAddress)
-        if (num === max) {
-          db.events.on('replicated', () => {
-            console.log('Fully replicated db:', odbAddress)
-          })
+    if (!openDBs[address]) {
+      console.log('Opening db:', address)
+      openDBs[address] = await orbitdb.open(address)
+      openDBs[address].events.on('ready', () => {
+        sendResponse()
+      })
+      openDBs[address].load()
+      openDBs[address].events.on(
+        'replicate.progress',
+        (odbAddress, entryHash, entry, num, max) => {
+          console.log('Replicating entry:', entryHash)
+          console.log('On db:', odbAddress)
+          if (num === max) {
+            openDBs[address].events.on('replicated', () => {
+              console.log('Fully replicated db:', odbAddress)
+              publish('REPLICATED', address)
+            })
+          }
         }
-      }
-    )
+      )
+    } else {
+      sendResponse()
+    }
+
+    function sendResponse () {
+      const numEntries = openDBs[address]._oplog._length
+      publish('HAS_ENTRIES', address, numEntries)
+    }
+  }
+
+  function publish (type, odbAddress, data) {
+    let dataObj = { type, odbAddress }
+    if (type === 'HAS_ENTRIES') {
+      dataObj.numEntries = data
+    } else if (type === 'REPLICATED') {
+    }
+    pubsub.publish(PINNING_ROOM, dataObj)
   }
 
   async function onMessage (topic, data) {
     console.log(topic, data)
-    openRootDB(data.odbAddress)
+    if (!data.type || data.type === 'PIN_DB') {
+      openRootDB(data.odbAddress)
+    }
   }
 
   async function onNewPeer (topic, peer) {
