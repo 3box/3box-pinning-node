2489363e1ed0dd993fb1a9b5be959d35d9a13547
Cristóbal Castillo <cristobal.castillo@consensys.net>
2018-11-20 11:26:01 -0300

    correct ipfs path, delete redundant code

--- server.js
+++ server.js
@@ -7,13 +7,8 @@ const Pubsub = require('orbit-db-pubsub')
 const DaemonFactory = require('ipfsd-ctl')
 const fs = require('fs')
 
-const ipfsOptions = {
-  EXPERIMENTAL: {
-    pubsub: true
-  }
-}
 const ORBITDB_PATH = '/opt/orbitdb'
-const IPFS_PATH = '/Users/oed/.jsipfs'
+const IPFS_PATH = '/opt/ipfs'
 const PINNING_ROOM = '3box-pinning'
 
 let openDBs = {}
@@ -32,9 +27,10 @@ async function startIpfsDaemon () {
         // init repo if not initialized
         await new Promise((resolve, reject) => { ipfsd.init(resolve) })
         // start the daemon
-        await new Promise((resolve, reject) => { ipfsd.start(['--enable-pubsub-experiment'], (err) => {
-          if (err) reject(err)
-          resolve()
+        await new Promise((resolve, reject) => {
+          ipfsd.start(['--enable-pubsub-experiment'], (err) => {
+            if (err) reject(err)
+            resolve()
           })
         })
         resolve(ipfsd.api)
