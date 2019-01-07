cdf9e8d8648bb4ee40d44bb82272638a14e7689f
Cristóbal Castillo <cristobal.castillo@consensys.net>
2019-01-07 17:16:30 -0300

    send infra metrics every 30 minutes

--- src/node.js
+++ src/node.js
@@ -20,18 +20,25 @@ const DAYS15 = 60 * 60 * 24 * 15 // 15 day ttl
 
 const util = new Util(IPFS_PATH)
 
+function sendInfraMetrics () {
+  analytics.track({
+    event: 'infra_metrics',
+    anonymousId: '3box',
+    properties: {
+      'total_root_stores': util.getTotalRootStores(),
+      'ipfs_disk_size': util.getIPFSDiskUsage(),
+      'time': Date.now()
+    }
+  })
+}
+
 async function start () {
   const cache = new RedisCache({ host: REDIS_PATH }, DAYS15)
   const pinning = new Pinning(cache, IPFS_PATH, ORBITDB_PATH, analytics)
   const cacheService = new CacheService(cache, pinning, ADDRESS_SERVER_URL)
-  analytics.track({
-    event: 'PinningStart',
-    anonymousId: '3box',
-    properties: {
-      rootStoreNumber: util.getTotalRootStores(),
-      diskSize: util.getIPFSDiskUsage()
-    }
-  })
+
+  setInterval(sendInfraMetrics, 1800000)
+
   await pinning.start()
   cacheService.start()
 }
