fe72a459f9e9ffb939f3c2ea487f56c9dc6583a8
Cristóbal Castillo <cristobal.castillo@consensys.net>
2019-01-15 18:49:40 -0300

    unversion the real env variables files

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,13 @@ if [[ "$CIRCLE_BRANCH" == "feat/automate-deployment" ]]; then
   git pull origin develop
   npm i -g pm2
   npm i
+  cp .env.development.example .env.development
   pm2 restart node
 fi
 if [ "$CIRCLE_BRANCH" == "master" ]; then
   git pull origin master
   npm i -g pm2
   npm i
+  cp .env.production.example .env.production
   pm2 restart node
 fi
