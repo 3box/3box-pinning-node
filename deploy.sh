8ef0cf75001d92a660995589d4135b36e2cead7b
Cristóbal Castillo <cristobal.castillo@consensys.net>
2019-01-15 12:20:47 -0300

    install pm2 if it wasn't installed

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 if [[ "${CIRCLE_BRANCH}" == "develop" ]]; then
   git pull origin develop
+  npm i -g pm2
   npm i
-  cp .env.development .env
   pm2 restart server
 fi
 if [ "${CIRCLE_BRANCH}" == "master" ]; then
   git pull origin master
+  npm i -g pm2
   npm i
-  cp .env.production .env
   pm2 restart server
 fi
