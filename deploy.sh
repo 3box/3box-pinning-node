3653fd61af1a703cc62c7cd145bcb30aaba3e857
Cristóbal Castillo <cristobal.castillo@consensys.net>
2019-01-15 17:26:09 -0300

    try script on develop

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-if [[ "${CIRCLE_BRANCH}" == "develop" ]]; then
+#if [[ "${CIRCLE_BRANCH}" == "develop" ]]; then
   git pull origin develop
   npm i -g pm2
   npm i
   pm2 restart server
-fi
+#fi
 if [ "${CIRCLE_BRANCH}" == "master" ]; then
   git pull origin master
   npm i -g pm2
