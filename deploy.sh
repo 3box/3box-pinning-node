5af6220432a0f928ada26716597bef7bfbed0f0a
Cristóbal Castillo <cristobal.castillo@consensys.net>
2019-01-15 17:34:20 -0300

    try the whole flow

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
-#if [[ "${CIRCLE_BRANCH}" == "develop" ]]; then
+
+if [[ "$CIRCLE_BRANCH" == "feat/automate-deployment" ]]; then
   git pull origin develop
   npm i -g pm2
   npm i
-  pm2 restart server
-#fi
-if [ "${CIRCLE_BRANCH}" == "master" ]; then
+  pm2 restart node
+fi
+if [ "$CIRCLE_BRANCH" == "master" ]; then
   git pull origin master
   npm i -g pm2
   npm i
-  pm2 restart server
+  pm2 restart node
 fi
