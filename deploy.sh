e6704ed156e74e65feb1c7cd460855df4a59377c
Cristóbal Castillo <cristobal.castillo@consensys.net>
2019-01-16 11:38:50 -0300

    clean git fetch

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-
+echo $CIRCLE_BRANCH
 if [[ "$CIRCLE_BRANCH" == "feat/automate-deployment" ]]; then
+  git stash
+  git checkout -f develop
   git pull origin develop
   npm i -g pm2
   npm i
@@ -8,6 +10,8 @@ if [[ "$CIRCLE_BRANCH" == "feat/automate-deployment" ]]; then
   pm2 restart node
 fi
 if [ "$CIRCLE_BRANCH" == "master" ]; then
+  git stash
+  git checkout -f master
   git pull origin master
   npm i -g pm2
   npm i
