c4fef07d9f004f1f2b9a3816e6e1520f91e5d246
Cristóbal Castillo <1154738+c-castillo@users.noreply.github.com>
2019-01-17 16:54:02 -0300

    deployment automation (#53)
    - Deployment automation
    - Latest deps updated

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
-
-git pull origin master
-cp .env.production .env
-pm2 restart server
+cd ~/3box-pinning-server
+if [[ "$CIRCLE_BRANCH" == "develop" ]]; then
+  git stash
+  git checkout -f develop
+  git pull origin develop
+  npm i -g pm2
+  npm i
+  if [ ! -f .env.development ]; then
+    cp .env.development.example .env.development
+  fi
+  pm2 restart node
+fi
+if [ "$CIRCLE_BRANCH" == "master" ]; then
+  git stash
+  git checkout -f master
+  git pull origin master
+  npm i -g pm2
+  npm i
+  if [ ! -f .env.production ]; then
+    cp .env.production.example .env.production
+  fi
+  pm2 restart node
+fi
