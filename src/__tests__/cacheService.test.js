89552f192353c9ec7c4f32e055b84af1442d4a86
Joel Torstensson <oed3933@gmail.com>
2019-03-02 07:04:54 -0500

    feat(api): Add support to query public space data

--- src/__tests__/cacheService.test.js
+++ src/__tests__/cacheService.test.js
@@ -16,11 +16,21 @@ jest.mock('axios', () => {
 })
 const CacheService = require('../cacheService')
 const cache = {
-  read: jest.fn(() => { return { name: 'testName' } }),
+  read: jest.fn(key => {
+    if (key === 'rsa') {
+      return { name: 'testName' }
+    } else if (key === 'rsa_space1'){
+      return { data: 'testData' }
+    } else {
+      return ['space0', 'space1']
+    }
+  }),
   write: jest.fn()
 }
 const pinning = {
-  getProfile: jest.fn(() => { return { name: 'testName' } })
+  getProfile: jest.fn(() => { return { name: 'testName' } }),
+  getSpace: jest.fn(() => { return { data: 'testData' } }),
+  listSpaces: jest.fn(() => { return ['space1', 'space2'] })
 }
 
 const ADDRESS_SERVER_URL = 'address-server'
@@ -36,7 +46,7 @@ describe('CacheService', () => {
   it('constructor works as expected', async () => {
     cs = new CacheService(cache, pinning, ADDRESS_SERVER_URL)
     expect(cs.app.use).toHaveBeenCalledTimes(1)
-    expect(cs.app.get).toHaveBeenCalledTimes(1)
+    expect(cs.app.get).toHaveBeenCalledTimes(3)
     expect(cs.app.post).toHaveBeenCalledTimes(1)
   })
 
@@ -72,6 +82,64 @@ describe('CacheService', () => {
     expect(res.json).toHaveBeenCalledWith({ name: 'testName' })
     expect(cs.cache.write).toHaveBeenCalledTimes(1)
     expect(cs.cache.write).toHaveBeenCalledWith('rsa', { name: 'testName' })
+  })
+
+  it('should get space correctly, with cache', async () => {
+    const req = { query: { address: '0x12345', name: 'space1' } }
+    const res = { json: jest.fn() }
+    await cs.getSpace(req, res)
+
+    expect(cs.cache.read).toHaveBeenCalledTimes(1)
+    expect(cs.cache.read).toHaveBeenCalledWith('rsa_space1')
+    expect(cs.pinning.getSpace).toHaveBeenCalledTimes(0)
+    expect(res.json).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledWith({ data: 'testData' })
+    expect(cs.cache.write).toHaveBeenCalledTimes(0)
+  })
+
+  it('should get space correctly, without cache', async () => {
+    const req = { query: { address: '0x12345', name: 'space1' } }
+    const res = { json: jest.fn() }
+    cache.read.mockImplementationOnce(() => null)
+    await cs.getSpace(req, res)
+
+    expect(cs.cache.read).toHaveBeenCalledTimes(1)
+    expect(cs.cache.read).toHaveBeenCalledWith('rsa_space1')
+    expect(cs.pinning.getSpace).toHaveBeenCalledTimes(1)
+    expect(cs.pinning.getSpace).toHaveBeenCalledWith('rsa', 'space1')
+    expect(res.json).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledWith({ data: 'testData' })
+    expect(cs.cache.write).toHaveBeenCalledTimes(1)
+    expect(cs.cache.write).toHaveBeenCalledWith('rsa_space1', { data: 'testData' })
+  })
+
+  it('should list spaces correctly, with cache', async () => {
+    const req = { query: { address: '0x12345' } }
+    const res = { json: jest.fn() }
+    await cs.listSpaces(req, res)
+
+    expect(cs.cache.read).toHaveBeenCalledTimes(1)
+    expect(cs.cache.read).toHaveBeenCalledWith('space-list_rsa')
+    expect(cs.pinning.listSpaces).toHaveBeenCalledTimes(0)
+    expect(res.json).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledWith(['space0', 'space1'])
+    expect(cs.cache.write).toHaveBeenCalledTimes(0)
+  })
+
+  it('should list spaces correctly, without cache', async () => {
+    const req = { query: { address: '0x12345' } }
+    const res = { json: jest.fn() }
+    cache.read.mockImplementationOnce(() => null)
+    await cs.listSpaces(req, res)
+
+    expect(cs.cache.read).toHaveBeenCalledTimes(1)
+    expect(cs.cache.read).toHaveBeenCalledWith('space-list_rsa')
+    expect(cs.pinning.listSpaces).toHaveBeenCalledTimes(1)
+    expect(cs.pinning.listSpaces).toHaveBeenCalledWith('rsa')
+    expect(res.json).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledWith(['space1', 'space2'])
+    expect(cs.cache.write).toHaveBeenCalledTimes(1)
+    expect(cs.cache.write).toHaveBeenCalledWith('space-list_rsa', ['space1', 'space2'])
   })
 
   it.skip('should get profiles correctly, with cache', async () => {
