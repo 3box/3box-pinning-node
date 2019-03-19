b02351031b83c1b01e4569696ae5202ac2f10c7d
Laurent Senta <Laurent.Senta@gmail.com>
2019-03-19 10:58:15 +0100

    did: extract signing key from did

--- src/util.js
+++ src/util.js
@@ -3,6 +3,9 @@ const elliptic = require('elliptic')
 const getSize = require('get-folder-size')
 const Multihash = require('multihashes')
 const sha256 = require('js-sha256').sha256
+const { InvalidDIDFormat } = require('./errors')
+
+const RE_DID_MUPORT = /^did:muport:(\s+)$/
 
 /**
   *  Collection of utilities to measure important KPIs
@@ -52,6 +55,23 @@ class Util {
   static uncompressSECP256K1Key(key) {
     const ec = new elliptic.ec('secp256k1')
     return ec.keyFromPublic(key, 'hex').getPublic(false, 'hex')
+  }
+
+
+  static didExtractIPFSAddress(did) {
+    const match = did.match(RE_DID_MUPORT)
+
+    if (!match) {
+      throw InvalidDIDFormat(did)
+    }
+
+    return match[1]
+  }
+
+  static async didExtractSigningKey(manifestIPFSAddr, ipfs) {
+    const content = await ipfs.get(manifestIPFSAddr)
+    const data = JSON.parse(content.toString())
+    return data.signingKey
   }
 }
 module.exports = Util
