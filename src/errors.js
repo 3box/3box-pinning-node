dfb97e4b519fcdcbe5383327122fc984134338bf
Joel Torstensson <oed3933@gmail.com>
2019-04-11 13:21:15 +0200

    Fix: pin dids on openDB and resolve them with did-resolver.

--- src/errors.js
+++ src/errors.js
@@ -10,14 +10,7 @@ const ProfileNotFound = (message) => {
   return err
 }
 
-const InvalidDIDFormat = (did) => {
-  const err = new Error(`Invalid DID Format, expected "did:muport:Qm....", got: ${did}`)
-  err.statusCode = 400
-  return err
-}
-
 module.exports = {
   InvalidInputError,
-  ProfileNotFound,
-  InvalidDIDFormat
+  ProfileNotFound
 }
