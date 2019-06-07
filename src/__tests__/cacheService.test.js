7790ccebb7cd99382a6a60b3edc82b616092d5fb
Joel Torstensson <oed3933@gmail.com>
2019-06-07 11:59:04 +0200

    feat(cacheService): get thread by address

--- src/__tests__/cacheService.test.js
+++ src/__tests__/cacheService.test.js
@@ -33,7 +33,7 @@ const cache = {
       return PROFILE_1
     } else if (key === 'rsa_space1') {
       return SPACE_1
-    } else if (key.startsWith('3box.thread')) {
+    } else if (key.startsWith('/orbitdb/')) {
       return THREADS_1
     } else {
       return SPACES_1
@@ -46,7 +46,8 @@ const pinning = {
   getProfile: jest.fn(() => PROFILE_1),
   getSpace: jest.fn(() => SPACE_1),
   listSpaces: jest.fn(() => SPACES_1),
-  getThread: jest.fn(() => THREADS_1)
+  getThread: jest.fn(() => THREADS_1),
+  getThreadAddress: jest.fn(() => '/orbitdb/Qm34534/name')
 }
 
 describe('CacheService', () => {
@@ -159,13 +160,17 @@ describe('CacheService', () => {
   })
 
   describe('Threads', () => {
+    beforeEach(() => {
+      pinning.getThreadAddress.mockClear()
+    })
+
     it('should get thread correctly, with cache', async () => {
       const req = { query: { space: 'space1', name: 'thread1', mod: 'did:3:asdfe', members: 'false' } }
       const res = { json: jest.fn() }
       await cs.getThread(req, res)
 
       expect(cs.cache.read).toHaveBeenCalledTimes(1)
-      expect(cs.cache.read).toHaveBeenCalledWith('3box.thread.space1.thread1')
+      expect(cs.cache.read).toHaveBeenCalledWith('/orbitdb/Qm34534/name')
       expect(cs.pinning.getThread).toHaveBeenCalledTimes(0)
       expect(res.json).toHaveBeenCalledTimes(1)
       expect(res.json).toHaveBeenCalledWith(THREADS_1)
@@ -179,13 +184,15 @@ describe('CacheService', () => {
       await cs.getThread(req, res)
 
       expect(cs.cache.read).toHaveBeenCalledTimes(1)
-      expect(cs.cache.read).toHaveBeenCalledWith('3box.thread.space1.thread1')
+      expect(cs.cache.read).toHaveBeenCalledWith('/orbitdb/Qm34534/name')
+      expect(cs.pinning.getThreadAddress).toHaveBeenCalledTimes(1)
+      expect(cs.pinning.getThreadAddress).toHaveBeenCalledWith('3box.thread.space1.thread1', 'did:3:asdfe', false)
       expect(cs.pinning.getThread).toHaveBeenCalledTimes(1)
-      expect(cs.pinning.getThread).toHaveBeenCalledWith('3box.thread.space1.thread1', 'did:3:asdfe', false)
+      expect(cs.pinning.getThread).toHaveBeenCalledWith('/orbitdb/Qm34534/name')
       expect(res.json).toHaveBeenCalledTimes(1)
       expect(res.json).toHaveBeenCalledWith(THREADS_1)
       expect(cs.cache.write).toHaveBeenCalledTimes(1)
-      expect(cs.cache.write).toHaveBeenCalledWith('3box.thread.space1.thread1', THREADS_1)
+      expect(cs.cache.write).toHaveBeenCalledWith('/orbitdb/Qm34534/name', THREADS_1)
     })
   })
 
