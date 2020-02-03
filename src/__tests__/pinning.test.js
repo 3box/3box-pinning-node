df03abdfe7271643a4c9548dabfd1399a5255ee0
Michael Sterle-Contala <2691351+msterle@users.noreply.github.com>
2020-02-03 12:53:50 +0100

    proper setup and teardown for each test to make them independent of each other

--- src/__tests__/pinning.test.js
+++ src/__tests__/pinning.test.js
@@ -1,123 +1,110 @@
-const OrbitDB = require('orbit-db')
-const Pubsub = require('orbit-db-pubsub')
-const {
-  OdbIdentityProvider,
-  LegacyIPFS3BoxAccessController,
-  ThreadAccessController,
-  ModeratorAccessController
-} = require('3box-orbitdb-plugins')
-const Identities = require('orbit-db-identity-provider')
-Identities.addIdentityProvider(OdbIdentityProvider)
-const AccessControllers = require('orbit-db-access-controllers')
-AccessControllers.addAccessController({ AccessController: LegacyIPFS3BoxAccessController })
-AccessControllers.addAccessController({ AccessController: ThreadAccessController })
-AccessControllers.addAccessController({ AccessController: ModeratorAccessController })
-const didJWT = require('did-jwt')
-const { registerMethod } = require('did-resolver')
-const { makeIPFS } = require('./tools')
-
 const Pinning = require('../pinning')
 
-const PINNING_ROOM = '3box-pinning'
-const IPFS_PATH_1 = './tmp/ipfs1'
-const IPFS_PATH_2 = './tmp/ipfs2'
-const ODB_PATH_1 = './tmp/orbitdb1'
-const ODB_PATH_2 = './tmp/orbitdb2'
-
-// Data to be pushed to the store
-const PUBLIC_NAME = { timeStamp: 12000, value: 'very name' }
-const PUBLIC_IMAGE = { timeStamp: 13000, value: 'such picture' }
-const PRIVATE_SHH = { timeStamp: 14000, value: 'many secret' }
-const PRIVATE_QUIET = { timeStamp: 15000, value: 'wow!' }
-
-// const PROFILE_ONLY_VALUES = {
-//   name: PUBLIC_NAME.value,
-//   image: PUBLIC_IMAGE.value
-// }
-
-// const PRIV_IMG_ONLY_VALUES = {
-//   quiet: PRIVATE_QUIET.value,
-//   shh: PRIVATE_SHH.value
-// }
-
-const THREEID_MOCK = {
-  DID: 'did:3:asdfasdf',
-  getKeyringBySpaceName: () => {
-    return {
-      getPublicKeys: () => {
-        return { signingKey: '044f5c08e2150b618264c4794d99a22238bf60f1133a7f563e74fcf55ddb16748159872687a613545c65567d2b7a4d4e3ac03763e1d9a5fcfe512a371faa48a781' }
-      }
-    }
-  },
-  signJWT: payload => {
-    return didJWT.createJWT(payload, {
-      signer: didJWT.SimpleSigner('95838ece1ac686bde68823b21ce9f564bc536eebb9c3500fa6da81f17086a6be'),
-      issuer: 'did:3:asdfasdf'
-    })
-  }
-}
-// we need to have a fake 3id resolver since we have a fake 3id
-const register3idResolver = () => registerMethod('3', async () => {
-  return {
-    '@context': 'https://w3id.org/did/v1',
-    id: 'did:3:asdfasdf',
-    publicKey: [{
-      id: 'did:3:asdfasdf#signingKey',
-      type: 'Secp256k1VerificationKey2018',
-      publicKeyHex: '044f5c08e2150b618264c4794d99a22238bf60f1133a7f563e74fcf55ddb16748159872687a613545c65567d2b7a4d4e3ac03763e1d9a5fcfe512a371faa48a781'
-    }],
-    authentication: [{
-      type: 'Secp256k1SignatureAuthentication2018',
-      publicKey: 'did:3:asdfasdf#signingKey'
-    }]
-  }
-})
-
-const cache = {
-  write: jest.fn()
-}
+const defaultsDeep = require('lodash.defaultsdeep')
+const path = require('path')
+const tmp = require('tmp-promise')
+tmp.setGracefulCleanup()
 
 jest.mock('redis', () => { return require('redis-mock') })
+const TestClient = require('./testClient')
+const { registerMock3idResolver } = require('./mock3id')
+
+// Needed for ipfs spinup/teardown
+jest.setTimeout(15000)
+
+const pinningIpfsConfig = {
+  Bootstrap: [],
+  Addresses: {
+    Swarm: [
+      '/ip4/127.0.0.1/tcp/4002',
+      '/ip4/127.0.0.1/tcp/4003/ws'
+    ]
+  }
+}
+
+const analyticsMock = {
+  trackPinDB: jest.fn(),
+  trackSyncDB: jest.fn(),
+  trackSpaceUpdate: jest.fn(),
+  trackPublicUpdate: jest.fn(),
+  trackRootUpdate: jest.fn(),
+  trackThreadUpdate: jest.fn(),
+  trackPrivateUpdate: jest.fn(),
+  trackPinDBAddress: jest.fn(),
+  trackSpaceUpdateByApp: jest.fn()
+}
+
+const mockProfileData = {
+  public: {
+    name: { timeStamp: 12000, value: 'very name' },
+    image: { timeStamp: 13000, value: 'such picture' }
+  },
+  private: {
+    shh: { timeStamp: 14000, value: 'many secret' },
+    quiet: { timeStamp: 15000, value: 'wow!' }
+  }
+}
+
+const mockThreadEntries = [
+  { message: 'a great post' },
+  { message: 'another great post' }
+]
+
+async function closeAllPinningNodeStores (pinning) {
+  const promises = Object.keys(pinning.openDBs).map(async key => {
+    await pinning.openDBs[key].db.close()
+    delete pinning.openDBs[key]
+  })
+  await Promise.all(promises)
+}
 
 describe('Pinning', () => {
+  let tmpDir
   let pinning
   let testClient
-  let analyticsMock
+  let clientIpfsOpts
 
-  jest.setTimeout(30000)
+  const pinningRoom = 'test-pinning-room'
 
   beforeAll(async () => {
-    analyticsMock = {
-      trackPinDB: jest.fn(),
-      trackSyncDB: jest.fn(),
-      trackSpaceUpdate: jest.fn(),
-      trackPublicUpdate: jest.fn(),
-      trackRootUpdate: jest.fn(),
-      trackThreadUpdate: jest.fn(),
-      trackPrivateUpdate: jest.fn(),
-      trackPinDBAddress: jest.fn(),
-      trackSpaceUpdateByApp: jest.fn()
-    }
-    pinning = new Pinning({ repo: IPFS_PATH_1 }, ODB_PATH_1, analyticsMock, undefined, undefined, PINNING_ROOM)
-    testClient = new TestClient()
-    testClient.onMsg = jest.fn()
-
-    pinning._dbOpenedBefore = jest.fn().mockReturnValue(false)
-    await Promise.all([pinning.start(), testClient.init()])
-
-    register3idResolver()
+    // await registerMock3idResolver()
   })
 
-  beforeEach(() => {
-    testClient.onMsg.mockClear()
-    cache.write.mockClear()
+  beforeEach(async () => {
+    tmpDir = await tmp.dir({ unsafeCleanup: true })
+    const orbitdbPath = tmpDir.path + '/orbitdb'
+    const ipfsPath = tmpDir.path + '/ipfs'
+    const ipfsOpts = {
+      config: pinningIpfsConfig,
+      repo: ipfsPath
+    }
+    const orbitCacheOpts = null
+    const pubSubConfig = null
+    const entriesNumCacheOpts = null
+    const pinWhitelistDids = null
+    const pinWhitelistSpaces = null
+    const pinSilent = null
+
+    pinning = new Pinning(ipfsOpts, orbitdbPath, analyticsMock, orbitCacheOpts, pubSubConfig, pinningRoom, entriesNumCacheOpts, pinWhitelistDids, pinWhitelistSpaces, pinSilent)
+    await pinning.start()
+    const pinningAddresses = await pinning.ipfs.swarm.localAddrs()
+    clientIpfsOpts = { config: { Bootstrap: pinningAddresses } }
+    testClient = new TestClient(clientIpfsOpts, pinningRoom)
+    await testClient.init()
+    await registerMock3idResolver()
+  })
+
+  afterEach(async () => {
+    await testClient.cleanup()
+    await pinning.stop()
+    await tmpDir.cleanup()
   })
 
   it('should sync db correctly from client', async () => {
-    await testClient.createDB(true)
+    await testClient.createDB(mockProfileData)
     const responsesPromise = new Promise((resolve, reject) => {
       const hasResponses = []
-      testClient.onMsg.mockImplementation((topic, data) => {
+      testClient.onMsg = (topic, data) => {
         if (data.type === 'HAS_ENTRIES') {
           expect(data.numEntries).toEqual(0)
           if (hasResponses.indexOf(data.odbAddress) === -1) {
@@ -130,47 +117,87 @@ describe('Pinning', () => {
           expect(hasResponses).toContain(testClient.privStore.address.toString())
           resolve()
         }
-      })
+      }
     })
-    testClient.announceDB()
+    await testClient.announceDB()
     await responsesPromise
-    // wait for stores to sync
-    await new Promise((resolve, reject) => { setTimeout(resolve, 3000) })
-    // TODO
-    // expect(cache.write).toHaveBeenCalledWith('space-list_' + testClient.rootStore.address.toString())
   })
 
-  // TODO
-  // it('should sync db correctly to client', async () => {
-  //   await testClient.reset()
-  //   await closeAllStores(pinning)
-  //   await testClient.createDB(false)
-  //   const responsesPromise = new Promise((resolve, reject) => {
-  //     let hasResponses = []
-  //     testClient.onMsg.mockImplementation((topic, data) => {
-  //       if (data.type === 'HAS_ENTRIES') {
-  //         expect(data.numEntries).toEqual(2)
-  //         hasResponses.push(data.odbAddress)
-  //       }
-  //       if (hasResponses.length === 3) {
-  //         expect(hasResponses).toContain(testClient.rootStore.address.toString())
-  //         expect(hasResponses).toContain(testClient.pubStore.address.toString())
-  //         expect(hasResponses).toContain(testClient.privStore.address.toString())
-  //         resolve()
-  //       }
-  //     })
-  //   })
-  //   await new Promise((resolve, reject) => { setTimeout(resolve, 5000) })
-  //   const dbSyncPromise = testClient.syncDB()
-  //   testClient.announceDB()
-  //   await responsesPromise
-  //   await dbSyncPromise
-  //
-  //   expect(await testClient.getProfile()).toEqual(PROFILE_ONLY_VALUES)
-  //   expect(await testClient.getPrivImg()).toEqual(PRIV_IMG_ONLY_VALUES)
-  // })
+  it('should sync db correctly to client', async () => {
+    // -- Create databases on the pinning node using the test client
+    await testClient.createDB(mockProfileData)
+    const responsesPromise = new Promise((resolve, reject) => {
+      const hasResponses = []
+      testClient.onMsg = (topic, data) => {
+        if (data.type === 'HAS_ENTRIES') {
+          if (hasResponses.indexOf(data.odbAddress) === -1) {
+            hasResponses.push(data.odbAddress)
+          }
+        }
+        if (hasResponses.length === 3) {
+          resolve()
+        }
+      }
+    })
+    await testClient.announceDB()
+    await responsesPromise
+
+    // We have to wait manually for the remote db to sync all entries because we have no sync event
+    await new Promise(resolve => setTimeout(resolve, 3000))
+
+    // -- Create new client with no data
+    const client2IpfsOpts = defaultsDeep({
+      config: {
+        Addresses: {
+          Swarm: [
+            '/ip4/127.0.0.1/tcp/4106',
+            '/ip4/127.0.0.1/tcp/4107/ws'
+          ]
+        }
+      }
+    }, clientIpfsOpts)
+    const testClient2 = new TestClient(client2IpfsOpts, pinningRoom)
+    await testClient2.init()
+
+    // -- Sync new client to pinning node
+    await testClient2.createDB()
+    await testClient2.announceDB()
+    await testClient2.storeSynced()
+
+    const expectedProfile = Object.keys(mockProfileData.public).reduce((acc, key) => {
+      acc[key] = mockProfileData.public[key].value
+      return acc
+    }, {})
+    const expectedPrivate = Object.keys(mockProfileData.private).reduce((acc, key) => {
+      acc[key] = mockProfileData.private[key].value
+      return acc
+    }, {})
+    expect(await testClient2.getProfile()).toEqual(expectedProfile)
+    expect(await testClient2.getPrivate()).toEqual(expectedPrivate)
+    testClient2.cleanup()
+  }, 30000)
 
   it('dbs should close after 30 min, but not before', async () => {
+    await testClient.createDB(mockProfileData)
+    const responsesPromise = new Promise((resolve, reject) => {
+      const hasResponses = []
+      testClient.onMsg = (topic, data) => {
+        if (data.type === 'HAS_ENTRIES') {
+          if (hasResponses.indexOf(data.odbAddress) === -1) {
+            hasResponses.push(data.odbAddress)
+          }
+        }
+        if (hasResponses.length === 3) {
+          resolve()
+        }
+      }
+    })
+    await testClient.announceDB()
+    await responsesPromise
+
+    // We have to wait manually for the remote db to sync all entries because we have no sync event
+    await new Promise(resolve => setTimeout(resolve, 3000))
+
     pinning.checkAndCloseDBs()
     let numOpenDBs = Object.keys(pinning.openDBs).length
     expect(numOpenDBs).toEqual(3)
@@ -192,207 +219,87 @@ describe('Pinning', () => {
   })
 
   describe('Threads', () => {
-    it('should pin thread correctly from client', async () => {
-      await testClient.createThread(true)
+    beforeEach(async () => {
+      await testClient.createDB(mockProfileData)
       const responsesPromise = new Promise((resolve, reject) => {
-        testClient.onMsg.mockImplementation((topic, data) => {
+        const hasResponses = []
+        testClient.onMsg = (topic, data) => {
+          if (data.type === 'HAS_ENTRIES') {
+            if (hasResponses.indexOf(data.odbAddress) === -1) {
+              hasResponses.push(data.odbAddress)
+            }
+          }
+          if (hasResponses.length === 3) {
+            resolve()
+          }
+        }
+      })
+      await testClient.announceDB()
+      await responsesPromise
+
+      // We have to wait manually for the remote db to sync all entries because we have no sync event
+      await new Promise(resolve => setTimeout(resolve, 3000))
+    })
+
+    it('should pin thread correctly from client', async () => {
+      await testClient.createThread(mockThreadEntries)
+      const responsesPromise = new Promise((resolve, reject) => {
+        testClient.onMsg = (topic, data) => {
           if (data.type === 'HAS_ENTRIES') {
             expect(data.numEntries).toEqual(0)
             resolve()
           }
-        })
+        }
       })
-      testClient.announceThread()
+      await testClient.announceThread()
       await responsesPromise
       // wait for thread to sync
       await new Promise((resolve, reject) => { setTimeout(resolve, 5000) })
     })
 
     it('should sync pinned data to client', async () => {
-      await closeAllStores(pinning)
-      await testClient.createThread(false)
+      // -- Create thread on the pinning node using the test client
+      await testClient.createThread(mockThreadEntries)
       const responsesPromise = new Promise((resolve, reject) => {
-        testClient.onMsg.mockImplementation((topic, data) => {
+        testClient.onMsg = (topic, data) => {
           if (data.type === 'HAS_ENTRIES') {
-            expect(data.numEntries).toEqual(2)
             resolve()
           }
-        })
+        }
       })
-      const dbSyncPromise = testClient.syncDB(true)
-      testClient.announceThread()
+      await testClient.announceThread()
       await responsesPromise
-      await dbSyncPromise
-      // for some reason there is an issue with the db not getting fully
-      // replicated in time even after the dbSyncPromise. Wait for 0.5 s
-      await new Promise((resolve, reject) => { setTimeout(resolve, 500) })
-      const posts = await testClient.getThreadPosts()
-      expect(posts[0].message).toEqual('a great post')
-      expect(posts[1].message).toEqual('another great post')
-    })
+
+      // We have to wait manually for the remote db to sync all entries because we have no sync event
+      await new Promise(resolve => setTimeout(resolve, 3000))
+
+      // -- Create new client with no data
+      const client2IpfsOpts = defaultsDeep({
+        config: {
+          Addresses: {
+            Swarm: [
+              '/ip4/127.0.0.1/tcp/4106',
+              '/ip4/127.0.0.1/tcp/4107/ws'
+            ]
+          }
+        }
+      }, clientIpfsOpts)
+      const testClient2 = new TestClient(client2IpfsOpts, pinningRoom)
+      await testClient2.init()
+      await testClient2.createDB()
+      await testClient2.announceDB()
+      await testClient2.createThread()
+      let posts = await testClient2.getThreadPosts()
+      expect(posts).toHaveLength(0)
+
+      // -- Sync new client to pinning node
+      await testClient2.createThread()
+      await testClient2.announceThread()
+      await testClient2.storeSynced({ thread: true })
+      posts = await testClient2.getThreadPosts()
+      expect(posts[0].message).toEqual(mockThreadEntries[0].message)
+      expect(posts[1].message).toEqual(mockThreadEntries[1].message)
+      testClient2.cleanup()
+    }, 30000)
   })
 })
-
-const closeAllStores = async pinning => {
-  const promises = Object.keys(pinning.openDBs).map(async key => {
-    await pinning.openDBs[key].db.close()
-    delete pinning.openDBs[key]
-  })
-  await Promise.all(promises)
-}
-
-class TestClient {
-  constructor () {
-    this.onMsg = () => {}
-  }
-
-  async init () {
-    this.ipfs = await initIPFS()
-    this.identity = await Identities.createIdentity({
-      type: '3ID',
-      threeId: THREEID_MOCK,
-      identityKeysPath: './tmp/odbIdentityKeys'
-    })
-  }
-
-  async createDB (withData) {
-    const ipfsId = await this.ipfs.id()
-    this.orbitdb = await OrbitDB.createInstance(this.ipfs, {
-      directory: ODB_PATH_2,
-      identity: this.identity
-    })
-    this.pubsub = new Pubsub(this.ipfs, ipfsId.id)
-    const key = THREEID_MOCK.getKeyringBySpaceName().getPublicKeys(true).signingKey
-    const opts = {
-      format: 'dag-pb',
-      accessController: {
-        write: [key],
-        type: 'legacy-ipfs-3box',
-        skipManifest: true
-      }
-    }
-    this.rootStore = await this.orbitdb.feed('rs.root', opts)
-    this.pubStore = await this.orbitdb.keyvalue('test.public', opts)
-    this.privStore = await this.orbitdb.keyvalue('test.private', opts)
-    await this.rootStore.add({ odbAddress: this.pubStore.address.toString() })
-    await this.rootStore.add({ odbAddress: this.privStore.address.toString() })
-    if (withData) {
-      await this.pubStore.put('name', PUBLIC_NAME)
-      await this.pubStore.put('image', PUBLIC_IMAGE)
-      await this.privStore.put('shh', PRIVATE_SHH)
-      await this.privStore.put('quiet', PRIVATE_QUIET)
-    }
-  }
-
-  announceDB () {
-    const rootStoreAddress = this.rootStore.address.toString()
-    this.pubsub.subscribe(PINNING_ROOM, this.onMsg.bind(this), () => {
-      this.pubsub.publish(PINNING_ROOM, { type: 'PIN_DB', odbAddress: rootStoreAddress })
-    })
-  }
-
-  async syncDB (thread) {
-    const syncStore = async store => {
-      return new Promise((resolve, reject) => {
-        store.events.on('replicate.progress',
-          (odbAddress, entryHash, entry, num, max) => {
-            if (num === max) {
-              store.events.on('replicated', () => {
-                resolve()
-              })
-            }
-          }
-        )
-      })
-    }
-    if (thread) {
-      await syncStore(this.thread)
-    } else {
-      await Promise.all([
-        syncStore(this.pubStore),
-        syncStore(this.privStore)
-      ])
-    }
-  }
-
-  async createThread (withData) {
-    const tName = '3box.thread.myspace.coolthread'
-    this.thread = await this.orbitdb.feed(tName, {
-      identity: this.identity,
-      accessController: {
-        type: 'thread-access',
-        threadName: tName,
-        members: false,
-        firstModerator: THREEID_MOCK.DID,
-        identity: this.identity
-      }
-    })
-    if (withData) {
-      await this.thread.add({ message: 'a great post' })
-      await this.thread.add({ message: 'another great post' })
-    }
-  }
-
-  async dropThread () {
-    await this.thread.drop()
-  }
-
-  announceThread () {
-    const address = this.thread.address.toString()
-    this.pubsub.publish(PINNING_ROOM, { type: 'SYNC_DB', odbAddress: address, thread: true })
-  }
-
-  async getThreadPosts () {
-    return this.thread
-      .iterator({ limit: -1 })
-      .collect().map(entry => {
-        const post = Object.assign({ postId: entry.hash }, entry.payload.value)
-        return post
-      })
-  }
-
-  async getProfile () {
-    const profile = this.pubStore.all
-    const parsedProfile = {}
-    Object.keys(profile).map(key => { parsedProfile[key] = profile[key].value })
-    return parsedProfile
-  }
-
-  async getPrivImg () {
-    const img = this.privStore.all
-    const parsedProfile = {}
-    Object.keys(img).map(key => { parsedProfile[key] = img[key].value })
-    return parsedProfile
-  }
-
-  async reset () {
-    await Promise.all([
-      this.rootStore.drop(),
-      this.pubStore.drop(),
-      this.privStore.drop()
-    ])
-    await this.orbitdb.stop()
-    await this.pubsub.disconnect()
-  }
-}
-
-const CONF = {
-  EXPERIMENTAL: {
-    pubsub: true
-  },
-  repo: IPFS_PATH_2,
-  config: {
-    Addresses: {
-      Swarm: [
-        '/ip4/127.0.0.1/tcp/4006',
-        '/ip4/127.0.0.1/tcp/4007/ws'
-      ],
-      API: '/ip4/127.0.0.1/tcp/5004',
-      Gateway: '/ip4/127.0.0.1/tcp/9092'
-    }
-  }
-}
-
-const initIPFS = async () => {
-  return makeIPFS(CONF)
-}
